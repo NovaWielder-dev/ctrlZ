b5cf818ab6a7e2f2151cc2a79cedbf1c46605570
Wian Mostert <wian.mostert@gmail.com>
2026-04-18 13:30:46 +0200

    Completed main

--- main.cpp
+++ main.cpp
@@ -14,14 +14,38 @@ int main() {
     std::vector<Action> all_actions;
 
     //Load data from .json
-    extractConfig("1.txt", my_car, my_race);
+    extractConfig("1.txt");
     std::cout << "Data loaded successfully!" << std::endl;
 
     //Create optimizer
     PandaOptimizer driver;
 
-    //Loop through laps and segments
-    
+    //Loop through laps
+    for (int lap = 1; lap <= my_race.total_laps; ++lap) {
+        
+        //Loop through segments
+        for (size_t i = 0; i < my_race.segments.size(); ++i) {
+            
+            Segment current = my_race.segments[i];
 
+            Segment next;
+            if (i + 1 < my_race.segments.size()) {
+                next = my_race.segments[i + 1];
+            }
+            else {
+                next = my_race.segments[0];
+            }
+
+            Action a = driver.optimizeSegment(my_car, current, next);
+
+            all_actions.push_back(a);
+        }
+    }
+
+    //[Trickster: Export final JSON]
+    //writeSubmission("submission.txt", all_actions);
+
+    std::cout << "Race Complete! Output saved." << std::endl;
+    return 0;
 
 }
